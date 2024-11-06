c52e9f89fb3b19e4d4cbcb9779023d6cdce886c5
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-11-06 00:58:25 -0500

    chore: upgrade `thiserror` (#15)
    Updated `thiserror` to version 2 and adjusted related features. Simplified conversion implementations in `Error` by using `#[from]` attribute on variants.

--- src/error.rs
+++ src/error.rs
@@ -1,31 +1,18 @@
+#![allow(clippy::missing_inline_in_public_items)]
+
 use alloy::{contract::Error as ContractError, sol_types::Error as AbiError};
 
-#[derive(Debug)]
-#[cfg_attr(feature = "std", derive(thiserror::Error))]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
     /// An error occurred retrieving the revert data.
-    #[cfg_attr(feature = "std", error("Invalid revert data"))]
+    #[error("Invalid revert data")]
     InvalidRevertData,
 
     /// An error occurred ABI encoding or decoding.
-    #[cfg_attr(feature = "std", error("{0}"))]
-    AbiError(AbiError),
+    #[error("{0}")]
+    AbiError(#[from] AbiError),
 
     /// An error occurred interacting with a contract over RPC.
-    #[cfg_attr(feature = "std", error("{0}"))]
-    ContractError(ContractError),
-}
-
-impl From<AbiError> for Error {
-    #[inline]
-    fn from(e: AbiError) -> Self {
-        Self::AbiError(e)
-    }
-}
-
-impl From<ContractError> for Error {
-    #[inline]
-    fn from(e: ContractError) -> Self {
-        Self::ContractError(e)
-    }
+    #[error("{0}")]
+    ContractError(#[from] ContractError),
 }
